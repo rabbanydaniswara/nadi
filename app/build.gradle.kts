d04d0e8999fc29599e1e1e74284c9c3dcafe921a
rabbanydaniswara <daniswara.rabbany79@gmail.com>
2026-06-26 20:48:06 +0700

    tambah konfigurasi tanda tangan release pakai jks keystore resmi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("nadi-release.jks")
+            storePassword = "nadi123"
+            keyAlias = "nadi-key"
+            keyPassword = "nadi123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -27,6 +36,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
